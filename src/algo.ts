95f78ff1df589122b9dbf97d6a28f38695103e37
Tasos Bitsios <tasosbitsios@algorandfndation>
2024-08-21 19:48:52 +0300

    retry fetch block

--- src/algo.ts
+++ src/algo.ts
@@ -1,8 +1,15 @@
 import algosdk from 'algosdk';
+import { sleep } from './utils.js';
 
 export async function getBlockProposer(algod: algosdk.Algodv2, rnd: number): Promise<string> {
-  const { cert: { prop: { oprop } } } = await algod.block(rnd).do();
-  return algosdk.encodeAddress(oprop);
+  try {
+    const { cert: { prop: { oprop } } } = await algod.block(rnd).do();
+    return algosdk.encodeAddress(oprop);
+  } catch(e) {
+    console.error(`Fetch ${rnd} failed: ${(e as Error).message}`);
+    await sleep(2000);
+    return getBlockProposer(algod, rnd);
+  }
 }
 
 export async function getLastRound(algod: algosdk.Algodv2): Promise<number> {
