7b9dbef37b2e4382e55b5d622675d7814f2dd96a
Tasos Bitsios <tasosbitsios@algorandfndation>
2024-08-21 16:33:33 +0300

    configurable algod

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,7 @@
 import algosdk from 'algosdk';
 
-// export const algod = new algosdk.Algodv2("7f52d8708c17404ebbc4880711110d89543f096cbb1806f24c11b13e93918183", "http://127.0.0.1", "8184");
-export const algod = new algosdk.Algodv2("", "https://fnet-api.d13.co", "443");
+const TOKEN = process.env.ALGOD_TOKEN ?? "";
+const HOST = process.env.ALGOD_HOST ?? "https://fnet-api.d13.co";
+const PORT = process.env.ALGOD_PORT ?? "443";
+
+export const algod = new algosdk.Algodv2(TOKEN, HOST, PORT);
