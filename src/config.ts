5440a5438e0a760df132023009f40e941bfe5055
Tasos Bitsios <tasosbitsios@algorandfndation>
2024-09-11 23:53:07 +0300

    100K vote threshold

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ const PORT = process.env.ALGOD_PORT ?? "443";
 
 export const algod = new algosdk.Algodv2(TOKEN, HOST, PORT);
 
-export const VOTE_ROUNDS_THRESHOLD = parseEnvInt("VOTE_ROUNDS_THRESHOLD", 10_000);
+export const VOTE_ROUNDS_THRESHOLD = parseEnvInt("VOTE_ROUNDS_THRESHOLD", 100_000);
 export const DB_CHUNKS = parseEnvInt("DB_CHUNKS", 100);
 export const NET_CONCURRENCY = parseEnvInt("CONCURRENCY", 10);
 export const SYNC_THRESHOLD = parseEnvInt("SYNC_THRESHOLD", 10);
